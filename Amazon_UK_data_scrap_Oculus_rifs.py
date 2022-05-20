016d21ab213f082f8a6a3743d705a9e827fb4b80
asi1117 <luy984361@gmail.com>
2022-05-20 19:23:13 +0900

    亞馬遜數據

---
+++ Amazon_UK_data_scrap_Oculus_rifs.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2022年5月20日
+主要爬取英國亞馬遜關於oculus——rifs的評論
+@author: luyijun
+"""
+
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import csv
+
+"""
+Created on 2022年5月19日
+
+@author: 盧一鈞
+"""
+
+# %%% relevent packages & modules
+
+import gc
+import os
+import pandas as pd
+import numpy as np
+import re
+import shelve
+import time
+import datetime
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import  traceback
+# %%% relevent website
+
+website = 'https://www.amazon.co.uk/product-reviews/B07PTMKYS7/ref=cm_cr_othr_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+
+# %%% initialize chrome
+# open website
+driver = Chrome()
+driver.get(website)
+driver.maximize_window()
+
+time_star = time.time()
+# %%% collect all reviews
+reviews_one_store = []
+condition_to_continue = True
+dataframe = pd.DataFrame()
+while (condition_to_continue):
+    WebDriverWait(driver, 10).until(
+        # 爬取一頁的所有的相關數據
+        EC.visibility_of_element_located((By.XPATH, "//div[@class='a-section review aok-relative']")))
+    reviews = driver.find_elements(by=By.XPATH, value="//div[@class='a-section review aok-relative']")
+
+    r = 0
+    count =0
+    try:
+        driver.find_element(by=By.XPATH, value='//*[@id="a-autoid-0"]').click()
+    except:
+        print('沒有cookie了')
+    # Finding all the reviews in the website and bringing them to python
+    for r in range(len(reviews)):
+
+        try:
+            soup = BeautifulSoup(reviews[r].get_attribute('innerHTML'), "html.parser")
+        except:
+            # I got an errorr saying that element is not attached to the page document
+            # To solve this I put an explicit wait condition that tells Selenium to wait until the element is available to be clicked on
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located((By.XPATH, "//div[@class='a-section review aok-relative']")))
+            # reviews = driver.find_elements_by_xpath("//li[@class='review-item']")
+            reviews = driver.find_elements(by=By.XPATH, value="//div[@class='a-section review aok-relative']")
+            soup = BeautifulSoup(reviews[r].get_attribute('innerHTML'), "html.parser")
+
+        # scrape raw html
+        try:
+            scrap_date = datetime.datetime.now()
+
+        except:
+            info = traceback.format_exc()
+            print(info)
+        try:
+            review_text = soup.find('span', attrs={'data-hook': 'review-body'}).text
+            print(review_text)
+        except:
+            review_text = ''
+            info = traceback.format_exc()
+            print(info)
+            print("評論文本這裏有問題")
+        try:
+            review_data = soup.find('span', attrs={'data-hook': 'review-date'}).text
+            print(review_data)
+        except:
+            review_data = ''
+            info = traceback.format_exc()
+            print(info)
+        try:
+            review_title = soup.find('a', attrs={'data-hook': 'review-title'}).text
+
+            print(review_title)
+        except:
+            review_title = soup.find('span', attrs={'data-hook': 'review-title'}).text
+            print('提取評論標題出異常')
+            info = traceback.format_exc()
+            print(info)
+        try:
+            review_rating = soup.find('i', attrs={'data-hook': 'review-star-rating'}).text.split('.')[0]
+            print(review_rating)
+        except:
+            review_rating = soup.find('i', attrs={'data-hook': 'cmps-review-star-rating'}).text.split('.')[0]
+            print('提取評分出異常')
+            info = traceback.format_exc()
+            print(info)
+        dataframe = dataframe.append(pd.DataFrame({
+            'scrapping_date': scrap_date,
+            'one_reviews_text': review_text,
+            'review_date': review_data,
+            'review_rating': review_rating,
+            'review_title':  review_title},
+            index=[count]))
+        count += 1
+        dataframe.to_csv("Amazon_UK_Oculus_rifs_reviews.csv", index=False, sep=',', encoding='utf_8_sig')
+
+    before = driver.page_source
+    driver.find_element(by=By.XPATH, value='//*[@id="cm_cr-pagination_bar"]/ul/li[2]/a').click()
+    after = driver.page_source
+    if before == after:
+        break
+    else:
+        time.sleep(10)
+
+# %%% data cleaning
+time_end = time.time()
+print('縂耗時', time_end - time_star, 's')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test2.py
+++ test2.py
@@ -1,7 +1,14 @@
+from bs4 import BeautifulSoup
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 web = Chrome()
-web.get('https://www.bestbuy.ca/en-ca/product/meta-quest-2-256gb-vr-headset-with-touch-controllers/15644387/review')
-web.find_element_by_xpath('//*[@id="root"]/div/div[2]/div[2]/div/div/div/div[2]/div/a/button').click()
+web.get('https://www.amazon.com/-/zh/product-reviews/B07PTMKYS7/ref=cm_cr_arp_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews')
+reviews = web.find_elements(by=By.XPATH, value="//div[@class='a-section review aok-relative']")
+print(len(reviews))
+r =0
+for r in range(len(reviews)):
+    soup = BeautifulSoup(reviews[r].get_attribute('innerHTML'), "html.parser")
+    review = soup.find('span',attrs = {'data-hook' : 'review-body'}).text
+    print(review)
